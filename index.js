24785523f7d8b113a2dd1f132e426781c867a019
Dan Jackson <dannyjacks2019@gmail.com>
2023-03-22 22:24:17 -0400

    Adds index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(){
+
+}
+function subtract(){
+
+}
+function multiply(){
+
+}
+function divide(){
+
+}
+function add(a, b){
+    return a + b
+}
+function subtract(a, b){
+    return (a - b)
+}
+function multiply(a, b){
+    return (a * b)
+}
+function divide(a, b){
+    return(a / b)
+}
+
+number = 10;
+
+function add5() {
+  return(number += 5);
+}
+function divideBy3(){
+    console.log(Dan);
+}
+divideBy3()
+
+function increment(n){
+    return(++n)
+}
+function decrement(n){
+    return(--n)
+}
+function makeInt(n){
+    return(n)
+}
+function makeInt(n){
+    return(parseInt(n, 10))
+}
+function preserveDecimal(n){
+    return(parseFloat(n, 10))
+}
